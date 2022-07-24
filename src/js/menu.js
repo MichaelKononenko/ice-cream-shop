1ba72ee9bd2c96a1e1a98d25b349964f7cbe1efe
LikeNikolai <nickolayfilip@gmail.com>
2022-07-24 18:32:10 +0300

    mob-menu autoclose on-click

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,10 +3,11 @@
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
     menu: document.querySelector('.mob-menu'),
-    body: document.querySelector('body'),
+    menu_list: document.querySelector('.mob-menu__list'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menu_list.addEventListener('click', toggleMenu);
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
