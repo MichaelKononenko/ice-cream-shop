a97dab53a6c2da245fd1c0d37af710012928e236
michael_kononenko <98155086+MichaelKononenko@users.noreply.github.com>
2022-07-25 12:57:24 +0300

    bug fix in about-modal

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -1,16 +1,16 @@
 (() => {
-    const refs = {
-      openAboutModalBtn: document.querySelector('.about__button'),
-      closeAboutModalBtn: document.querySelector('.about-modal__close-btn'),
-      aboutModal: document.querySelector('.about-backdrop'),
-      bodyAbout: document.querySelector('body'),
-    };
-  
-    refs.openAboutModalBtn.addEventListener("click", toggleModal);
-    refs.closeAboutModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.aboutModal.classList.toggle("is-hidden");
-      refs.bodyAbout.classList.toggle('about-modal--open');
-    }
-  })();
+  const refs = {
+    openAboutModalBtn: document.querySelector('.about__button'),
+    closeAboutModalBtn: document.querySelector('.about-modal__close-btn'),
+    aboutModal: document.querySelector('.about-backdrop'),
+    bodyAbout: document.querySelector('body'),
+  };
+
+  refs.openAboutModalBtn.addEventListener('click', toggleModal);
+  refs.closeAboutModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.aboutModal.classList.toggle('is-hidden');
+    refs.bodyAbout.classList.toggle('about-modal--open');
+  }
+})();
