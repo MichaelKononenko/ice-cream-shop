8a43f39488b471979d915a1c29bede4d4cef34a7
MichaelKononenko <98155086+MichaelKononenko@users.noreply.github.com>
2022-07-25 09:45:20 +0000

    Deploying to gh-pages from @ MichaelKononenko/ice-cream-shop@e120b83c6678831d93e0aec4694e21ad430102c6 🚀

---
+++ index.8475492a.js
@@ -0,0 +1,2 @@
+!function(){var o={openAboutModalBtn:document.querySelector(".about__button"),closeAboutModalBtn:document.querySelector(".about-modal__close-btn"),aboutModal:document.querySelector(".about-backdrop"),bodyAbout:document.querySelector("body")};function t(){o.aboutModal.classList.toggle("is-hidden"),o.bodyAbout.classList.toggle("about-modal--open")}o.openAboutModalBtn.addEventListener("click",t),o.closeAboutModalBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.8475492a.js.map
