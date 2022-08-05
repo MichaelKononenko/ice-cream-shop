4ca20cb44e6a66542b2bd61784d4e0a250d84bab
michael_kononenko <98155086+MichaelKononenko@users.noreply.github.com>
2022-08-05 21:26:04 +0300

    gallery fix

---
+++ src/js/cart.js
@@ -0,0 +1,11 @@
+const refs = {
+  cartIsVisible: document.querySelector('.header__bascket-icon'),
+};
+let qty = document.getElementById('firstCount');
+qty = qty.innerHTML;
+// qty = Number.qty;
+// if (qty.innerHTML < 1) {
+//   refs.cartIsVisible.classList.add('is-hiden');
+// }
+console.log(`qty = ${qty.value}`);
+document.getElementById('qty').value = this.innerHTML;
