41ec530fa029b245ed998ac12b6177de1b975b3d
MichaelKononenko <98155086+MichaelKononenko@users.noreply.github.com>
2022-08-31 10:57:16 +0000

    Deploying to dev from @ MichaelKononenko/ice-cream-shop@1afee7aa82b6836c376a021ee09f84c56f8c70c7 🚀

--- index.ec6a9d7a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".header__bascket-icon");let e=document.getElementById("firstCount");e=e.innerHTML,console.log(`qty = ${e.value}`),document.getElementById("qty").value=$915de8ffc4dd3149$export$8bd3fff6c675ca8d;
-//# sourceMappingURL=index.ec6a9d7a.js.map
